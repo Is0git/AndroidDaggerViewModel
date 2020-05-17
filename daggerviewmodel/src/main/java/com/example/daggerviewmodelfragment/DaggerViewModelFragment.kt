29ab1ceb5c97c930586b9b83f61972708606ada6
Danielius <dnlspasin@gmail.com>
2020-05-18 00:06:37 +0300

    init

---
+++ daggerviewmodel/src/main/java/com/example/daggerviewmodelfragment/DaggerViewModelFragment.kt
@@ -0,0 +1,28 @@
+package com.example.daggerviewmodelfragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import dagger.android.support.DaggerFragment
+import javax.inject.Inject
+
+abstract class DaggerViewModelFragment<T : ViewModel>(var clazz: Class<T>) : DaggerFragment() {
+
+    lateinit var viewModel: T
+
+    @Inject
+    lateinit var factory: ViewModelFactory
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = createViewModel()
+    }
+
+    private fun createViewModel(): T {
+        return ViewModelProvider(this, factory).get(clazz)
+    }
+
+}
